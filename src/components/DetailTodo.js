1ea4221148e3c036a55ca036451da7f35f5ba8bd
ReturnReason <reasonz@daum.net>
2022-10-27 01:58:06 +0900

    design todo 디테일 페이지 스타일 컴포넌트 적용

--- src/components/DetailTodo.js
+++ src/components/DetailTodo.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 function Detailtodo({ todos, selectedTodoId }) {
   const currentTodo = todos.find((todo) => {
     return todo.id === selectedTodoId;
   });
 
-  console.log(currentTodo);
-
   return (
     <TodoDetailContainer>
-      <div>{currentTodo.task}</div>
-      <p>{currentTodo.memo}</p>
+      <DetailTitle>{currentTodo.task}</DetailTitle>
+      <DetailMemoContainer>
+        <DetailMemo>{currentTodo.memo}</DetailMemo>
+      </DetailMemoContainer>
+      <CloseBtn>CANCLE</CloseBtn>
     </TodoDetailContainer>
   );
 }
@@ -31,6 +32,76 @@ const TodoDetailContainer = styled.div`
     border-radius: 20px;
     resize: none;
   }
+`;
+
+const DetailTitle = styled.div`
+  font-family: 'GmarketSansMedium';
+  margin: 0 auto;
+  position: relative;
+  border-radius: 15px;
+  border: 4px solid #94a8ff65;
+  width: 480px;
+  height: 55px;
+  padding-left: 25px;
+  padding-right: 25px;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 2px 5px 10px #b5c3ff3b;
+  margin-bottom: 20px;
+  transition: all 0.2s;
+  font-size: 20px;
+`;
+
+const DetailMemoContainer = styled(DetailTitle)`
+  height: 100%;
+  min-height: 300px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  heigth: 100%;
+  align-items: flex-start;
+  padding-top: 15px;
+`;
+
+const DetailMemo = styled.p`
+  font-family: 'GmarketSansMedium';
+
+  font-size: 20px;
+`;
+
+const CloseBtn = styled.button`
+  width: 480px;
+  background: #94a7ff;
+  border-radius: 200px;
+  box-shadow: 5px 10px 20px #7990ff40;
+  transition: all 0.5s;
+  cursor: pointer;
+  overflow: hidden;
+  padding: 10px;
+  border: none;
+  color: #fff;
+  font-size: 20px;
+
+  &:hover {
+    background: #a9b9ff;
+  }
+
+  &:active {
+    background: #b29ff3;
+  }
+
+  ${(props) =>
+    props.showCreateTodo &&
+    css`
+      transform: rotate(45deg);
+      background: #b29ff3;
+
+      &:hover {
+        background: #9e8ae6;
+      }
+    `}
 `;
 
 export default Detailtodo;
